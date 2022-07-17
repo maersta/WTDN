5f381b662f0747bcadd10d5884690de57bd5436f
maersta <40675809+maersta@users.noreply.github.com>
2022-07-18 01:49:23 +0200

    Create regex.py

---
+++ scripts/regex.py
@@ -0,0 +1,9 @@
+import re
+with open ('tmpscreen/tmpocr.txt', 'r' ) as f:
+    content = f.read()
+    content_new = re.sub('\w+(.+):', r'', content, flags = re.M)
+
+f = open("tmpscreen/final.txt", "w")
+f.write(content_new)
+f.close()
+print("Done")

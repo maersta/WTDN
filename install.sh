d7c3521b68060d07b978492c38e0ebbd0fb16d71
maersta <40675809+maersta@users.noreply.github.com>
2022-07-17 18:54:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,12 +18,13 @@ echo " Installing ADB tools.."
 echo -e "\n"
 apt-get update -y && apt-get upgrade -y
 apt-get install -y adb
+mkdir tmpscreen
 clear
 echo -e "\e[32m 000000000000000000000000000000000000000000000000\e[0m"
 echo -e "\e[1;32m WTDN Installer v1.0 - BY MAERSTA          \e[0m"
 echo -e "\e[32m 000000000000000000000000000000000000000000000000\e[0m"
 echo -e "\n"
-read -p " Installation completed! Press ctrl+c to exit and then sudo bash wtdn.sh" completed
+read -p " Installation completed! Press ctrl+c to exit and then sudo bash main.sh" completed
 fi
 clear
 echo -e "\e[32m 000000000000000000000000000000000000000000000000\e[0m"

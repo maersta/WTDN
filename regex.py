992af31e80c9ce6dfdf8d8821212e93e9d861241
maersta <40675809+maersta@users.noreply.github.com>
2022-07-18 15:51:00 +0200

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,8 +1,5 @@
 import re
 import sys
-print sys.argv[0] 
-print sys.argv[1] 
-print sys.argv[2]
 with open ('tmpscreen/tmp.txt', 'r' ) as f:
     content = f.read()
     content_new = re.sub('\w+', r'HORAN', content, flags = re.M)

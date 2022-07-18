e1e44ca0bdf20ca2a080945a0b146041865ab304
maersta <40675809+maersta@users.noreply.github.com>
2022-07-17 20:38:58 -0400

    Delete regex.py

--- scripts/regex.py
+++
@@ -1,8 +0,0 @@
-import re
-with open ('tmpscreen/tmp.txt', 'r' ) as f:
-    content = f.read()
-    content_new = re.sub('\w+', r'HORAN', content, flags = re.M)
-
-f = open("tmpscreen/final.txt", "w")
-f.write(content_new)
-f.close()
